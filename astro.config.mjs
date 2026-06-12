1d819c89e6d67cd6be9546e1e40c4f7bcaf25f7c
cemil <cemil3456@icloud.com>
2026-06-12 12:45:09 +0200

    Update astro.config.mjs to define site and base URL for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://cemil2006.github.io',
+  base: 'stage_opdracht_vrcafe',
+});
